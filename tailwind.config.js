042c55c5a42843c1945686b3ec9471ba1be28c4d
smartkhon2712 <smartkhon2712@gmail.com>
2024-05-10 23:47:00 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    colors: {
+      white: "#FFFFFF",
+      "purple-600": "#7E3AF2",
+      "gray-50": "#F9FAFB",
+      "gray-200": "#E5E7EB",
+      "gray-500": "#6B7280",
+      "gray-800": "#1F2A37",
+      "gray-900": "#111928",
+    },
+  },
+};
